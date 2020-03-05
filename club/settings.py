b83d7a8ed982096bae31d914ab234afc354c3205
xfhc@9495@163.com <xfhc9495@163.com>
2020-03-05 11:03:27 +0800

    completed mysql config

--- club/settings.py
+++ club/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,18 +72,22 @@ WSGI_APPLICATION = 'club.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+MYSQL_HOST = os.environ.get("MYSQL_HOST", 'localhost')
+MYSQL_PORT = os.environ.get('MYSQL_PORT', '3306')
+MYSQL_NAME = os.environ.get('MYSQL_NAME', 'club')
+
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'club',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': MYSQL_NAME,
+        'USER': MYSQL_USER,
+        'PASSWORD': MYSQL_PASSWORD,
+        'HOST': MYSQL_HOST,
+        'PORT': MYSQL_PORT,
+        'TEST': {"NAME": "test_mysql"}
     }
 }
 
